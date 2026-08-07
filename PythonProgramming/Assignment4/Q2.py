e44606e89ef6f98114c525d71f946a5d6fac0863
Ranajit Bera <ranajitbera321@gmail.com>
2026-08-07 19:01:23 +0530

    Q2.py modified

--- PythonProgramming/Assignment4/Q2.py
+++ PythonProgramming/Assignment4/Q2.py
@@ -2,6 +2,14 @@
 li = []
 size = int(input("Enter the size of the list "))
 print("Enter the elements of the list ")
+found = 0
 for i in range(size):
   li.append(int(input()))
 num = int(input("Enter a number to find "))
+for i in li:
+  if i == num:
+    found+=1
+if found>0:
+  print(f"The number {num} is found.")
+else:
+  print("The number is not found.")
